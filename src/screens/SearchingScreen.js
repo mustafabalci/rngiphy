d81bc4af2dbcc91e9487e649f90969dd33152d02
Mustafa BALCI <balcimustafa1@gmail.com>
2021-03-25 23:01:30 +0300

    chore: create mock screens

---
+++ src/screens/SearchingScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const SearchingScreen = () => {
+  return (
+    <View>
+      <Text>SearchingScreen</Text>
+    </View>
+  );
+};
+
+export default SearchingScreen;
---
+++ src/screens/TrendingScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const TrendingScreen = () => {
+  return (
+    <View>
+      <Text>TrendingScreen</Text>
+    </View>
+  );
+};
+
+export default TrendingScreen;
