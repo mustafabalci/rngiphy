0465a71558a950ebfb75b68fc2bd1a6adfb0a579
Mustafa BALCI <balcimustafa1@gmail.com>
2021-03-28 02:55:15 +0300

    feat: add data provider

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { Stack } from '@navigation';
+import DataProvider from './src/contexts/DataContext';
 
 const App = () => {
-  return <Stack />;
+  return (
+    <DataProvider>
+      <Stack />
+    </DataProvider>
+  );
 };
 
 export default App;
