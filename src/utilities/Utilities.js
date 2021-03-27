c86872b5e4df4d6642de694161c5bce33c9636da
Mustafa BALCI <balcimustafa1@gmail.com>
2021-03-27 14:35:42 +0300

    chore: refactor screen size utilities

--- src/utilities/Utilities.js
+++ src/utilities/Utilities.js
@@ -1,3 +1,4 @@
 import { Dimensions } from 'react-native';
 
-export const { width, height } = Dimensions.get('window');
+export const windowWidth = Dimensions.get('window').width;
+export const windowHeight = Dimensions.get('window').height;
