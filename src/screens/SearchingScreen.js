a9d17bb8f2af2fbcaa652f56a6b05ecf21df39a2
Mustafa BALCI <balcimustafa1@gmail.com>
2021-03-28 04:53:23 +0300

    feat: add ui and functionality to searching screen

--- src/screens/SearchingScreen.js
+++ src/screens/SearchingScreen.js
@@ -1,12 +1,58 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useContext, useEffect, useState } from 'react';
+import { FlatList, RefreshControl, StyleSheet } from 'react-native';
+import { Heading, PageWrapper, SearchInput, ListItem } from '../components';
+import { DataContext } from '../contexts/DataContext';
 
-const SearchingScreen = () => {
+const SearchingScreen = ({ navigation }) => {
+  const {
+    getSearchingGifs,
+    searchingGifs,
+    fetching,
+    setSearchingGifs,
+  } = useContext(DataContext);
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      getSearchingGifs();
+    });
+
+    return unsubscribe;
+  }, [navigation]);
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('blur', () => {
+      setSearchingGifs([]);
+    });
+
+    return unsubscribe;
+  }, [navigation]);
+
+  const renderItem = ({ item }) => (
+    <ListItem sourceUri={item.images.fixed_height.url} />
+  );
+
   return (
-    <View>
-      <Text>SearchingScreen</Text>
-    </View>
+    <PageWrapper>
+      <SearchInput onSubmit={() => getSearchingGifs()} />
+      <Heading text="Results" />
+      <FlatList
+        data={searchingGifs}
+        renderItem={renderItem}
+        keyExtractor={gif => gif.id}
+        numColumns={3}
+        refreshControl={
+          <RefreshControl refreshing={fetching} onRefresh={getSearchingGifs} />
+        }
+        style={styles.flatList}
+      />
+    </PageWrapper>
   );
 };
+
+const styles = StyleSheet.create({
+  flatList: {
+    marginHorizontal: -8,
+  },
+});
 
 export default SearchingScreen;
