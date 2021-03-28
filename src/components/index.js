ea29f1dc6848ecb05bb242f601608a6fc9304df7
Mustafa BALCI <balcimustafa1@gmail.com>
2021-03-28 04:51:57 +0300

    chore: index new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
 export PageWrapper from './PageWrapper/PageWrapper';
 export ColorBox from './ColorBox/ColorBox';
 export Button from './Button/Button';
+export SearchInput from './SearchInput/SearchInput';
+export Heading from './Heading/Heading';
+export ListItem from './ListItem/ListItem';
