869c5da12871f64d9d03453fce0f59f9d6805d0c
Mustafa BALCI <balcimustafa1@gmail.com>
2021-03-28 05:46:38 +0300

    feat: add modal component

---
+++ src/components/InfoModal/InfoModal.js
@@ -0,0 +1,65 @@
+import React, { useContext } from 'react';
+import { View, Modal, StyleSheet, Text, Pressable } from 'react-native';
+import { UtilsContext } from '../../contexts/UtilsContext';
+
+const InfoModal = ({ show, message }) => {
+  const { setModalShow } = useContext(UtilsContext);
+
+  return (
+    <Modal animationType="slide" transparent={true} visible={show}>
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          <Text style={styles.modalText}>{message}</Text>
+          <Pressable
+            style={[styles.button, styles.buttonClose]}
+            onPress={() => setModalShow(false)}>
+            <Text style={styles.textStyle}>Kapat</Text>
+          </Pressable>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
+
+export default InfoModal;
